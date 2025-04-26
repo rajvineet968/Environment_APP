a08e874558a06876757287fd6f7a6e190b025dbc
Vineet <rajvineet968@gmail.com>
2025-04-26 10:13:20 +0530

    Added files

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const exp= require('express');
+const app=express();
+
+const port=5000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/',(req,res)=>{
+    res.render('/home.ejs');
+});
